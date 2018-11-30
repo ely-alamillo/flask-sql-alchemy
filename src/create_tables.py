98b11a7417e33f4063cd42b63a4b94ff4731a3c7
Ely Alamillo <ely0013@gmail.com>
2018-11-30 00:00:15 -0600

    add id to items table script

--- src/create_tables.py
+++ src/create_tables.py
@@ -7,7 +7,7 @@ create_users_table = "CREATE TABLE IF NOT EXISTS users\
 (id INTEGER PRIMARY KEY, username text, password text)"
 
 create_items_table = "CREATE TABLE IF NOT EXISTS items\
-(name text, price real)"
+(id INTEGER PRIMARY KEY, name text, price real)"
 
 cursor.execute(create_users_table)
 cursor.execute(create_items_table)
